b809d2a984f76db6aeee07c79137e0284ab89199
kthong3 <kthongsakounh@gmail.com>
2018-07-21 15:19:48 -0500

    update User routes, add and pass tests for signing up new users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :user
+  resources :users
+  get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
